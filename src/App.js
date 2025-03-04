fdc85cfb5f5befddb842563f724cf5a08a954045
DogYoJeong <dlgkstj20@naver.com>
2025-03-04 22:34:27 +0900

    두번째 목업

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 //import logo from './logo.svg';
 import './App.css';
 
+let lastScrollY = window.scrollY;
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > lastScrollY) {
+        // 아래로 스크롤하면 숨김
+        header.classList.add("hidden");
+    } else {
+        // 위로 스크롤하면 나타남
+        header.classList.remove("hidden");
+    }
+    lastScrollY = window.scrollY;
+});
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
       </header>
     </div>
   );
